67a16a3abb6a6268a4be38d38ebf37ceb7b36a3e
Dev <devsantoriya818@gmail.com>
2026-02-17 09:39:30 +0530

    Update deps and Vite build config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic',
+  })],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: {
+          'gsap': ['gsap'],
+          'framer-motion': ['framer-motion'],
+        }
+      }
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', 'gsap', 'framer-motion']
+  }
 })
